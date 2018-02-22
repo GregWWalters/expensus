0b582f1beef479134dca6083ac77f8d1e9bd0647
atreidesend <jmeek13@gmail.com>
2018-02-22 23:10:11 +0000

    case rename 3 ugh

--- lib/client/index.tsx
+++ lib/client/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { App } from './app'
+import { App } from './App'
 import configureStore from './state/store'
 
 const store = configureStore()
