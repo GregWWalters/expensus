80d84661d8b537209a4b90bd9620c497c385395c
atreidesend <jmeek13@gmail.com>
2018-02-28 21:58:41 +0000

    setup groups

--- lib/client/components/group/Group.tsx
+++ lib/client/components/group/Group.tsx
@@ -1,18 +1,53 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom'
+import { ApiState, RequestState } from '../../../types'
+import State from '../../../types/state'
+import { GroupForClient } from '../../../types/state/group'
+import {
+  selectGroup,
+  selectLoadGroupState,
+} from '../../state/selectors/groupState'
 import { FlexWindow } from '../shared/Layouts'
 import { GroupCreate } from './GroupCreate'
 
-type Props = RouteComponentProps<{}>
+interface StateProps {
+  loadGroupState: ApiState
+  group: GroupForClient | null
+}
 
-const Group: React.SFC<Props> = ({ match }) => (
-  <FlexWindow className="group" flex="column">
-    <Switch>
-      <Route path={`${match.url}/create`} component={GroupCreate} />
-      <Route path={`${match.url}/view`} render={() => <div>Group-view</div>} />
-      <Redirect to={`${match.url}/view`} />
-    </Switch>
-  </FlexWindow>
-)
+type Props = StateProps & RouteComponentProps<{}>
 
-export { Group }
+const Group: React.SFC<Props> = ({ group, loadGroupState, match }) => {
+  if (group) {
+    return (
+      <FlexWindow className="group" flex="column">
+        <Switch>
+          <Route
+            path={`${match.url}/view`}
+            render={() => <div>Group-view</div>}
+          />
+          <Redirect to={`${match.url}/view`} />
+        </Switch>
+      </FlexWindow>
+    )
+  } else if (loadGroupState.status === RequestState.REQUESTING) {
+    return <FlexWindow className="spinner spinner--dark" />
+  } else {
+    return (
+      <FlexWindow className="group" flex="column">
+        <Switch>
+          <Route path={`${match.url}/create`} component={GroupCreate} />
+          <Redirect to={`${match.url}/create`} />
+        </Switch>
+      </FlexWindow>
+    )
+  }
+}
+
+const connected = connect<StateProps, {}, {}, State>(state => ({
+  loadGroupState: selectLoadGroupState(state),
+  group: selectGroup(state),
+}))(Group)
+
+export { connected as Group }
