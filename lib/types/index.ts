1a9a17bdfe7ca8c46c9329b644cd57d76b20fff1
atreidesend <jmeek13@gmail.com>
2018-03-03 11:24:07 +0000

    more descriptive api state names

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -3,10 +3,10 @@ import { ClientApiError } from './api'
 export type Nullable<T> = T | null | undefined
 
 export enum RequestState {
-  NOT_REQUESTED,
-  REQUESTING,
-  COMPLETED,
-  ERROR,
+  NOT_REQUESTED = 'NOT_REQUESTED',
+  REQUESTING = 'REQUESTING',
+  COMPLETED = 'COMPLETED',
+  ERROR = 'ERROR',
 }
 
 export interface ApiState {
