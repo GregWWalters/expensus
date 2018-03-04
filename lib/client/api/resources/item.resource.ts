efaea2c80807d5440f62a50fd170d50a045df9c5
atreidesend <jmeek13@gmail.com>
2018-03-04 21:42:12 +0000

    client side item resource

---
+++ lib/client/api/resources/item.resource.ts
@@ -0,0 +1,32 @@
+import {
+  CreateItemParams,
+  CreateItemResponseBody,
+  GetItemsResponseBody,
+} from '../../../types/api/item.types'
+import { ApiResource } from '../ApiResource'
+
+export default class ItemResource extends ApiResource {
+  async createItem(params: CreateItemParams) {
+    try {
+      const resp = await this.req.post<CreateItemResponseBody>(
+        '/item/create',
+        params,
+        { headers: this.defaultHeaders }
+      )
+      return resp.data
+    } catch (err) {
+      return this.handleError(err)
+    }
+  }
+
+  async getItems() {
+    try {
+      const resp = await this.req.get<GetItemsResponseBody>('/item', {
+        headers: this.defaultHeaders,
+      })
+      return resp.data
+    } catch (err) {
+      return this.handleError(err)
+    }
+  }
+}
