151bdce7f3b5a640c7cbb9c7936678cafcbf11d9
atreidesend <jmeek13@gmail.com>
2018-04-02 15:41:29 +0100

    selectors and helpers for transaction state

---
+++ lib/client/state/selectors/transactionState.ts
@@ -0,0 +1,23 @@
+import { createSelector } from 'reselect'
+import State from '../../../types/state'
+import { TransactionForClient } from '../../../types/state/transaction'
+
+export const selectTransactionState = (state: State) => state.transactionState
+
+export const selectLoadTransactions = createSelector(
+  selectTransactionState,
+  transactionState => transactionState.loadTransactions
+)
+
+export const selectTransactions = createSelector(
+  selectTransactionState,
+  transactionState => transactionState.transactions
+)
+
+// === Transaction Helper functions
+export function isTransactionFullyAllocated(
+  txn: TransactionForClient
+): boolean {
+  if (!txn.allocations) return false
+  return txn.allocations.reduce((sum, al) => sum + al.amount, 0) === txn.amount
+}
